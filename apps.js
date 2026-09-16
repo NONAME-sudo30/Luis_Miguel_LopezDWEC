0f3596e0e3b46d577c577889e698c9dc300329fb
COMPUTADORA\luism <Luismiguel291107@gmail.com>
2026-09-16 17:13:31 +0200

    Cambios

--- apps.js
+++ apps.js
@@ -1,1 +1,13 @@
 console.log("¡Hola DWEC! Mi taller ya funciona.");s
+// Recuperar elementos por su id
+const boton = document.getElementById("boton");
+const aviso = document.getElementById("aviso");
+
+// Variable contador
+let contador = 0;
+
+// Escuchar el clic en el botón
+boton.addEventListener("click", () => {
+    contador++;
+    aviso.textContent = `Has hecho clic ${contador} veces`;
+});
