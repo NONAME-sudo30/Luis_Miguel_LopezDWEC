e800587ef1b03fde1aed24c2c745401a44dd7134
COMPUTADORA\luism <Luismiguel291107@gmail.com>
2026-09-16 17:36:10 +0200

    Añadiendo el modo noche

--- apps.js
+++ apps.js
@@ -1,4 +1,4 @@
-console.log("¡Hola DWEC! Mi taller ya funciona.");s
+console.log("¡Hola DWEC! Mi taller ya funciona.");  
 // Recuperar elementos por su id
 const boton = document.getElementById("boton");
 const aviso = document.getElementById("aviso");
@@ -10,4 +10,16 @@ let contador = 0;
 boton.addEventListener("click", () => {
     contador++;
     aviso.textContent = `Has hecho clic ${contador} veces`;
+    
+});
+const modonoche = document.getElementById("modonoche");
+
+modonoche.addEventListener("click", () => {
+  document.body.classList.toggle("tema-claro");
+
+  if (document.body.classList.contains("tema-claro")) {
+    boton.textContent = "☀️";
+  } else {
+    boton.textContent = "🌙";
+  }
 });
