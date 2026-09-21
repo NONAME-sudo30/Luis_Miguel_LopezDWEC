29d33883b4e2ca0d3bc044e27b930a886182436d
COMPUTADORA\luism <Luismiguel291107@gmail.com>
2026-09-21 17:30:40 +0200

    Añadir datos de juegos a la tabla en HTML y actualizar la estructura de filas

--- apps.js
+++ apps.js
@@ -22,4 +22,48 @@ modonoche.addEventListener("click", () => {
   } else {
     boton.textContent = "🌙";
   }
+ 
+const juegos = [
+  {
+    id: "fila1",
+    nombre: "Borderlands 4",
+    compañia: "Gearbox Software",
+    plataforma: "PC",
+    valoracion: 9.0,
+    precio: 29.99
+  },
+  {
+    id: "fila2",
+    nombre: "FC 27",
+    compañia: "EA Sports",
+    plataforma: "PS5",
+    valoracion: 8.0,
+    precio: 100
+  },
+  {
+    id: "fila3",
+    nombre: "Elden Ring",
+    compañia: "FromSoftware",
+    plataforma: "PC",
+    valoracion: 9.0,
+    precio: 59.99
+  },
+  {
+    id: "fila4",
+    nombre: "Hollow Knight: Silksong",
+    compañia: "Team Cherry",
+    plataforma: "PC",
+    valoracion: 9.5,
+    precio: 20
+  },
+  {
+    id: "fila5",
+    nombre: "Persona 5",
+    compañia: "Atlus",
+    plataforma: "PS5, PC",
+    valoracion: 9.0,
+    precio: 50
+  }
+];
+
 });
